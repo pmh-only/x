091c05aa4f348fceb3de5974edbbd0a3dfe67438
Minhyeok Park <pmh_only@pmh.codes>
2025-12-29 09:48:59 +0900

    feat: make k8s manifests met best practices

--- nuke.sh
+++ nuke.sh
@@ -3,4 +3,4 @@ if [[ ! -f ./_nuke/aws-nuke ]]; then
   wget $(curl https://api.github.com/repos/ekristen/aws-nuke/releases/latest | jq '.assets[]|select(.name|endswith("linux-amd64.tar.gz")).browser_download_url' -r) -O- | tar zxvf - -C ./_nuke
 fi
 
-_nuke/aws-nuke nuke -q --force --no-alias-check --no-dry-run -c _nuke/config.yaml
+_nuke/aws-nuke nuke -q --force --no-alias-check --no-dry-run -c _nuke/config.yaml $*
